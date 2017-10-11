b54e9f8667eb8a9277cfa8a0cd6a431e14e2643c
Andrey Kamakin <a.kamakin@icloud.com>
2017-10-11 12:41:15 +0300

    Changed default port

--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	port := flag.String("p", ":80", "a string")
+	port := flag.String("p", ":8080", "a string")
 	webRoot := flag.String("wr", "/var/www/html", "a string")
 	numCpu := flag.Int("c", 1, "an int")
 	flag.Parse()
