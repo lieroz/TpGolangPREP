1d2c24a62e7f46ae1fd8f59a024a2d266a16ece6
root <root@localhost.localdomain>
2017-11-27 19:48:34 +0300

    Added generic methods

--- 6/99_homework/db_crud/handler.go
+++ 6/99_homework/db_crud/handler.go
@@ -51,7 +51,7 @@ func (mhr *MyHander) GetTables(ctx *MyContext, baseResp *MyResponse) {
 
 func (mhr *MyHander) GetTableEntries(ctx *MyContext, baseResp *MyResponse) {
 	table := ctx.GetPathVar("table")
-	sqlQuery := "SELECT * FROM " + table
+	sqlQuery := fmt.Sprintf("SELECT * FROM %s", table)
 	var args []interface{}
 	if limit, ok := ctx.Request.URL.Query()["limit"]; ok {
 		sqlQuery += " LIMIT ?"
@@ -68,12 +68,32 @@ func (mhr *MyHander) GetTableEntries(ctx *MyContext, baseResp *MyResponse) {
 		baseResp.ServeError(ctx.Writer, http.StatusNotFound, err.Error())
 		return
 	}
+	cols, _ := rows.Columns()
 	var records []interface{}
-	switch table {
-	case "items":
-		records = getItems(rows)
-	case "users":
-		records = getUsers(rows)
+	for rows.Next() {
+		columns := make([]interface{}, len(cols))
+		columnPointers := make([]interface{}, len(cols))
+		for i := range columns {
+			columnPointers[i] = &columns[i]
+		}
+		rows.Scan(columnPointers...)
+		m := make(map[string]interface{})
+		for i, colName := range cols {
+			val := columnPointers[i].(*interface{})
+			m[colName] = *val
+		}
+		for k, v := range m {
+			if v != nil {
+				if k == "id" {
+					m[k], _ = strconv.Atoi(string(v.([]uint8)))
+				} else {
+					m[k] = string(v.([]uint8))
+				}
+			} else {
+				m[k] = nil
+			}
+		}
+		records = append(records, m)
 	}
 	response := make(map[string]interface{})
 	response["records"] = records
@@ -81,107 +101,76 @@ func (mhr *MyHander) GetTableEntries(ctx *MyContext, baseResp *MyResponse) {
 	baseResp.ServeSuccess(ctx.Writer, http.StatusOK)
 }
 
-func getItems(rows *sql.Rows) []interface{} {
-	records := make([]interface{}, 0)
-	for rows.Next() {
-		var item Item
-		rows.Scan(&item.ID, &item.Title, &item.Description, &item.Updated)
-		records = append(records, item)
-	}
-	return records
-}
-
-func getUsers(rows *sql.Rows) []interface{} {
-	records := make([]interface{}, 0)
-	for rows.Next() {
-		var user User
-		rows.Scan(&user.ID, &user.Login, &user.Password, &user.Email, &user.Info, &user.Updated)
-		records = append(records, user)
-	}
-	return records
-}
-
 func (mhr *MyHander) GetTableEntry(ctx *MyContext, baseResp *MyResponse) {
 	table := ctx.GetPathVar("table")
 	id, _ := strconv.Atoi(ctx.GetPathVar("id"))
 	sqlQuery := "SELECT * FROM %s WHERE id = ?"
 	row := mhr.DB.QueryRow(fmt.Sprintf(sqlQuery, table), id)
-	var record interface{}
-	var err error
-	switch table {
-	case "items":
-		record, err = getItem(row)
-	case "users":
-		record, err = getUser(row)
+	rows, _ := mhr.DB.Query(fmt.Sprintf(sqlQuery, table), id) // Needed to get columns information, can also be done in NewCrudDB
+	cols, _ := rows.Columns()
+	columns := make([]interface{}, len(cols))
+	columnPointers := make([]interface{}, len(cols))
+	for i := range columns {
+		columnPointers[i] = &columns[i]
 	}
-	if err != nil {
+	if err := row.Scan(columnPointers...); err != nil {
 		baseResp.ServeError(ctx.Writer, http.StatusNotFound, "record not found")
 		return
 	}
+	m := make(map[string]interface{})
+	for i, colName := range cols {
+		val := columnPointers[i].(*interface{})
+		m[colName] = *val
+	}
+	for k, v := range m {
+		if v != nil {
+			if k == "id" {
+				m[k], _ = strconv.Atoi(string(v.([]uint8)))
+			} else {
+				m[k] = string(v.([]uint8))
+			}
+		} else {
+			m[k] = nil
+		}
+	}
 	response := make(map[string]interface{})
-	response["record"] = record
+	response["record"] = m
 	baseResp.Body["response"] = response
 	baseResp.ServeSuccess(ctx.Writer, http.StatusOK)
-}
-
-func getItem(row *sql.Row) (interface{}, error) {
-	var item Item
-	err := row.Scan(&item.ID, &item.Title, &item.Description, &item.Updated)
-	return item, err
-}
-
-func getUser(row *sql.Row) (interface{}, error) {
-	var user User
-	err := row.Scan(&user.ID, &user.Login, &user.Password, &user.Email, &user.Info, &user.Updated)
-	return user, err
 }
 
 func (mhr *MyHander) CreateTableEntry(ctx *MyContext, baseResp *MyResponse) {
 	table := ctx.GetPathVar("table")
-	var id int64
-	var err error
-	switch table {
-	case "items":
-		id, err = createItem(mhr.DB, ctx)
-	case "users":
-		id, err = createUser(mhr.DB, ctx)
+	data := make(map[string]interface{})
+	defer ctx.Request.Body.Close()
+	body, _ := ioutil.ReadAll(ctx.Request.Body)
+	json.Unmarshal(body, &data)
+	sqlQuery := fmt.Sprintf("INSERT INTO %s (", table)
+	var args []interface{}
+	for k, v := range data {
+		if k == "id" {
+			continue
+		}
+		sqlQuery += fmt.Sprintf("%s,", k)
+		args = append(args, v)
 	}
+	sqlQuery = sqlQuery[:len(sqlQuery)-1]
+	sqlQuery += ") VALUES ("
+	for i := 0; i < len(args); i++ {
+		sqlQuery += "?,"
+	}
+	sqlQuery = sqlQuery[:len(sqlQuery)-1]
+	sqlQuery += ")"
+	res, err := mhr.DB.Exec(sqlQuery, args...)
 	if err != nil {
 		baseResp.ServeError(ctx.Writer, http.StatusNotFound, "error creating entity")
 		return
 	}
+	id, _ := res.LastInsertId()
 	response := make(map[string]interface{})
 	response["id"] = id
 	baseResp.Body["response"] = response
 	baseResp.ServeSuccess(ctx.Writer, http.StatusOK)
-}
-
-func createItem(db *sql.DB, ctx *MyContext) (int64, error) {
-	sqlQuery := "INSERT INTO items (title, description, updated) VALUES (?, ?, ?)"
-	item := new(Item)
-	body, _ := ioutil.ReadAll(ctx.Request.Body)
-	json.Unmarshal(body, item)
-	ctx.Request.Body.Close()
-	res, err := db.Exec(sqlQuery, item.Title, item.Description, item.Updated)
-	if err != nil {
-		return 0, err
-	}
-	id, _ := res.LastInsertId()
-	return id, err
-}
-
-func createUser(db *sql.DB, ctx *MyContext) (int64, error) {
-	sqlQuery := "INSERT INTO users (login, password, email, info, updated) VALUES (?, ?, ?, ?, ?)"
-	user := new(User)
-	body, _ := ioutil.ReadAll(ctx.Request.Body)
-	json.Unmarshal(body, user)
-	ctx.Request.Body.Close()
-	res, err := db.Exec(sqlQuery, user.Login, user.Password, user.Email, user.Info, user.Updated)
-	if err != nil {
-		return 0, err
-	}
-	id, _ := res.LastInsertId()
-	return id, err
 }
 
 func (mhr *MyHander) UpdateTableEntry(ctx *MyContext, baseResp *MyResponse) {
